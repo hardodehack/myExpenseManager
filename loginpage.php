f53afa35bc6b63f5bd8f219657ff2df51a701342
Hardik Patel <hnpatel6787@gmail.com>
2016-01-10 19:43:16 +0530

    LoginPage Upfated

--- loginpage.php
+++ loginpage.php
@@ -24,25 +24,72 @@ if($loggedin) {
 <html>
 
 <head>
-    <title> Expense Manager</title>
-    
-    </head>
-    
-    <body>
-    
-    <h1> Expense manager for friends!</h1>
-        
-        
-        <table>
-        
-            <tr>
-            
-            <td>Username :  </td> <td> <input type='text' name='uname' placeholder='Enter Your Username'/> </td>
-            </tr>
-        
-        </table>
-    
-    </body>
+	<title>Expence Manager</title>
+	<style>
+			#mainDiv{
+				margin:0;
+				padding:0;
+				width:100%;
+				height:100%;
+			}
+			.hh1
+				{
+					font-size:20px;
+					padding-top:3px;
+					padding-bottom:2px;
+				}
+			.t1
+				{
+					margin-top:200px;
+					margin-left:500px;
+					margin-right:500px;
+				}
+			.t2
+				{
+				
+					background:cyan;
+					border:2px solid black;
+					border-radius:10px;
+					padding:20px;
+				}
+				
+                            
+				
+	</style>
+			
+</head>
 
+<body>
+	<div id ="mainDiv">
+	<div class="hh1">
+		<marquee><font color="green">Welcome to Expence Manager</font></marquee>
+	</div>
+
+	<div class="t1">
+	<div class="t2">
+	<form method="post" action="loginaction.php">
+		<table>
+			<tr>
+				<td><label><font color="orange">Username </font></label></td>
+				<td></td>
+				<td><input placeholder="Enter your Username here" type="text" name="uname"/></td>
+			</tr>
+			<tr>
+				<td><label><font color="orange">Password </font></label></td>
+				<td></td>
+				<td><input type="password" placeholder="Enter your Password here" name="pass"/>
+			</tr>
+			<tr>
+				<td></td>
+				<td></td>
+				
+				<td><button type="submit">Submit</button>
+                        </tr>
+			</table>
+			</div>
+	</form>
+	</div>
+	
+</body>
 </html>
 
